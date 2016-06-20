af6555a4bf402f164fd2e4a1c4af3d7092f727a1
Kate Harwood <kateharwood>
2016-06-20 15:43:16 -0400

    added motion to bubbles

--- Bubble.js
+++ Bubble.js
@@ -6,10 +6,70 @@ import {
     Text,
     View,
     TouchableWithoutFeedback,
+    Animated,
 } from 'react-native';
 
+let { width, height} = require('Dimensions').get('window');
+let BUBBLE_SIZE = 40;
+let MAX_DELAY = 2000;
+let MIN_DELAY = 500;
+let MAX_SPEED = 8000;
+let MIN_SPEED = 5000;
 
 class Bubble extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            pan: new Animated.ValueXY(),
+        };
+    }
+
+    componentDidMount() {
+        this.startAndRepeat();
+    }
+   
+    // continuously repeat animation
+    startAndRepeat = () => {
+        this.triggerAnimation(this.startAndRepeat);
+    }
+
+    // animated bubble components
+    triggerAnimation = (cb) => {
+        Animated.sequence([
+            Animated.timing(this.state.pan, { // start bubbles at bottom
+                duration: 0,
+                toValue: {x: 0, y: height - 150}
+            }),
+            Animated.timing(this.state.pan, {
+                ...this.getTimeConfig(),
+                toValue: {x: 0, y: 0}
+            }),
+            Animated.timing(this.state.pan, { // bubbles move more slowly on way back down
+                duration: 10000,
+                toValue: {x: 0, y: height - 100}
+            })
+        ]).start(cb);
+    }
+
+    // get random durations for animation
+    getTimeConfig = () => {
+        return {
+            duration: Math.random() * (MAX_SPEED - MIN_SPEED) + MIN_SPEED, 
+           // delay: Math.random() * (MAX_DELAY - MIN_DELAY) + MIN_DELAY,
+        }
+    }
+
+    // update positions of bubbles
+    getStyle() {
+        return [
+            styles.bubble,
+            {
+                transform: this.state.pan.getTranslateTransform()
+            }
+        ];
+    }
+
+    // call pop bubble function when clicked on
     handlePress(){
         this.props.handlePress();
     }
@@ -17,9 +77,7 @@ class Bubble extends React.Component {
     render () {
         return (
             <TouchableWithoutFeedback onPress={() => this.handlePress()}>
-                <View style={styles.bubble}>
-                    <Text>{this.props.text}</Text>
-                </View>
+                <Animated.View style={this.getStyle()} />
             </TouchableWithoutFeedback> 
         )
     }
@@ -29,11 +87,11 @@ const styles = StyleSheet.create({
     bubble: {
         borderStyle: 'solid',
         borderWidth: .25,
-        width: 40,
-        height: 40,
+        width: BUBBLE_SIZE,
+        height: BUBBLE_SIZE,
         borderRadius: 100,
         backgroundColor: '#F0F8FF',
-        marginTop: 20,
+        marginRight: 5,
     }
 });
 
