b572be2388e7a9c323aee9889bebab2c9f468f57
Galen March <galenmarch@comcast.net>
2016-07-13 11:51:24 -0400

    new tweener, animatedsprite

--- components/Tweener.js
+++ components/Tweener.js
@@ -2,96 +2,7 @@
 
 import { Animated, Easing } from 'react-native';
 
-// ******* TWEENER *********
-// Template/instructions for adding new animations:
-//
-// const ANIMATION_NAME = function(options, state) {
-//    if (looping === false) {
-//       return;
-//    }
-//    Animated.timing/sequence/parallel/etc... (
-//      ***custom animation code goes here***
-//        animate state values like state.opacity
-//        or state.top/left using the Animated class
-//        in conjuction with information recieved from
-//        options like options.endXY or options.startXY
-//    ).start(() => {
-//       if (options.loop === false) {
-//          looping = false;
-//       }
-//       if (options.disappearAfterAnimation) {
-//          state.opacity.setValue(0);
-//          OR
-//          state.top.setValue(-500);
-//          (or anything that removes the component from view)
-//       }
-//      ANIMATION_NAME(options, state);
-//      });
-//  }
-//
-// Once a new animation is created in the manner of the one
-// above, go to the bottom of file to the return statement.
-// associate the name of your new animation with a reference
-// string ('reference':ANIMATION_NAME).  Then go to the Looper
-// function and a case to the switch statement like this:
-//   case 'reference':
-//       ANIMATION_NAME(options, state);
-//       break;
-//
-// *************************
-
 const Tweener = function () {
-
-  let looping = false;  // looping bool
-
-
-
-  const Looper = function(options, state, anim) {
-
-    if (options.loop === true) {
-      looping = !looping;
-    } else {
-        looping = true;
-    }
-
-    switch(anim) {
-      case 'move':
-          move(options, state);
-          break;
-      case 'pulse':
-          pulse(options, state);
-          break;
-      case 'wiggle':
-          wiggle(options, state);
-          break;
-      case 'bounce':
-          bounce(options, state);
-          break;
-      case 'sine-wave':
-          sineWave(options, state);
-          break;
-      case 'bounce-drop':
-          bounceDrop(options, state);
-          break;
-      case 'zoom':
-          zoom(options, state);
-          break;
-      case 'hop':
-          hop(options, state);
-          break;
-      case 'tumble-off':
-          tumbleOff(options, state);
-          break;
-      case 'spin':
-          spin(options, state);
-          break;
-      case 'hop-forward':
-          hopForward(options, state);
-          break;
-    }
-
-     }
-
 
   const bounce = function(options, state) {
     if (looping === false) {
@@ -108,18 +19,13 @@ const Tweener = function () {
       if (options.loop === false) {
         looping = false;
       }
-      if (options.disappearAfterAnimation) {
-        state.opacity.setValue(0);
-      }
       bounce(options, state);
     });
 
   };
 
   const move = function (options, state) {
-    if (looping === false) {
-      return;
-    }
+
     state.top.setValue(options.startXY[0]);
     Animated.timing(          // Uses easing functions
        state.top,    // The value to drive
@@ -136,19 +42,15 @@ const Tweener = function () {
         duration: 1000, }            // Configuration
       ).start(() => {
         if (options.loop === false) {
-          looping = false;
+          return
+        }else{
+          move(options, state);
         }
-        if (options.disappearAfterAnimation) {
-          state.opacity.setValue(0);
-        }
-        move(options, state);
       });
   };
 
   const sineWave = function(options, state){
-    if (looping === false) {
-      return;
-    }
+
     state.top.setValue(options.startXY[1]);
     state.left.setValue(options.startXY[0]);
     Animated.parallel(
@@ -158,24 +60,19 @@ const Tweener = function () {
       ]
     ).start(() => {
       if (options.loop === false) {
-        looping = false;
+        return
+      }else{
+        sineWave(options, state);
       }
-      if (options.disappearAfterAnimation) {
-        state.opacity.setValue(0);
-      }
-      sineWave(options, state);
+
     });
 
   }
-
-  // _getSequenceX and _getSequenceY are helper functions, used
-  // by sine-wave.
 
   const _getSequenceX = function (options, state) {
     const duration = options.duration;
     const numTrasitions = options.xTo.length;
     return options.xTo.map((x, index, array)=>{
-      console.log(`X = ${x}`);
       return Animated.timing(state.left, {
           duration: duration / numTrasitions,
           toValue: x,
@@ -196,314 +93,277 @@ const Tweener = function () {
       });
     }
 
-   const pulse = function(options, state) {
-     if (looping === false) {
-       return;
-     }
-     state.scale.setValue(1);
-     Animated.sequence([
-       Animated.timing(
-         state.scale,
-         {
-           toValue: 1.25,
-           easing: Easing.linear,
-           duration: 400,
-         }
-       ),
-       Animated.timing(
-         state.scale,
-         {
-           toValue: 1,
-           easing: Easing.linear,
-           duration: 400,
-         }
-       ),
-     ]).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         state.opacity.setValue(0);
-       }
-       pulse(options, state);
-     });
-   }
-
-   const wiggle = function(options, state) {
-     if (looping === false) {
-       return;
-     }
-     Animated.sequence([
+ const pulse = function(options, state) {
+   state.scale.setValue(1);
+   Animated.sequence([
      Animated.timing(
-       state.rotation,
+       state.scale,
        {
-         toValue: 3,
+         toValue: 1.25,
          easing: Easing.linear,
-         duration: 100,
+         duration: 400,
        }
      ),
      Animated.timing(
-       state.rotation,
+       state.scale,
        {
-         toValue: -3,
+         toValue: 1,
          easing: Easing.linear,
-         duration: 100,
-       }
-     ),
-     Animated.spring(
-       state.rotation,
-       {
-         toValue: 0,
-         friction: 1,
+         duration: 400,
        }
      ),
    ]).start(() => {
      if (options.loop === false) {
-       looping = false;
+       return
+     }else{
+       pulse(options, state);
      }
-     if (options.disappearAfterAnimation) {
+   });
+ }
+ const wiggle = function(options, state) {
+   Animated.sequence([
+   Animated.timing(
+     state.rotation,
+     {
+       toValue: 3,
+       easing: Easing.linear,
+       duration: 100,
+     }
+   ),
+   Animated.timing(
+     state.rotation,
+     {
+       toValue: -3,
+       easing: Easing.linear,
+       duration: 100,
+     }
+   ),
+   Animated.spring(
+     state.rotation,
+     {
+       toValue: 0,
+       friction: 1,
+     }
+   ),
+ ]).start(() => {
+   if (options.loop === false) {
+     return
+   }else{
+     wiggle(options, state);
+   }
+ });
+ }
+ const bounceDrop = function(options, state) {
+   state.left.setValue(options.startXY[0]);
+   Animated.timing(
+     state.left,
+     {
+       toValue: options.endXY[0],
+       easing: Easing.spring,
+       duration: options.duration,
+     }
+   ).start();
+   state.top.setValue(options.startXY[1]);
+   Animated.timing(
+     state.top,
+     {
+       toValue: options.endXY[1],
+       easing: Easing.bounce,
+       duration: options.duration,
+     }
+   ).start(() => {
+     // WHAT is this?????
+     if (options.destroy) {
        state.opacity.setValue(0);
      }
-     wiggle(options, state);
-   });
-   }
-
-   const bounceDrop = function(options, state) {
-     if (looping === false) {
-       return;
-     }
-     state.left.setValue(options.startXY[0]);
-     Animated.timing(
-       state.left,
-       {
-         toValue: options.endXY[0],
-         easing: Easing.spring,
-         duration: options.duration,
-       }
-     ).start();
-     state.top.setValue(options.startXY[1]);
-     Animated.timing(
-       state.top,
-       {
-         toValue: options.endXY[1],
-         easing: Easing.bounce,
-         duration: options.duration,
-       }
-     ).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         //state.opacity.setValue(0);
-         state.top.setValue(-500); // moves it out of the way of other views
-       }
+     if (options.loop === false) {
+       return
+     }else{
        bounceDrop(options, state);
-     });
-   }
-
-   const zoom = function(options, state) {
-     if (looping === false) {
-       return;
      }
-     state.left.setValue(options.startXY[0]);
-     Animated.timing(
-       state.left,
-       {
-         toValue: options.endXY[0],
-         easing: Easing.back(3),
-         duration: options.duration,
-       }
-     ).start();
-     state.top.setValue(options.startXY[1]);
-     Animated.timing(
-       state.top,
-       {
-         toValue: options.endXY[1],
-         easing: Easing.back(3),
-         duration: options.duration,
-       }
-     ).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         state.opacity.setValue(0);
-       }
-       zoom(options, state);
-     });
-   }
+   });
+ }
 
-   const hop = function(options, state) {
-     if (looping === false) {
-       return;
+ const zoom = function(options, state) {
+   state.left.setValue(options.startXY[0]);
+   Animated.timing(
+     state.left,
+     {
+       toValue: options.endXY[0],
+       easing: Easing.back(3),
+       duration: options.duration,
      }
-     state.top.setValue(options.startXY[1]);
-     Animated.sequence([
-       Animated.timing(
-         state.top,
-         {
-           toValue: (options.startXY[1] - 75),
-           easing: Easing.linear,
-           duration: 100,
-         }
-       ),
-       Animated.timing(
-         state.top,
-         {
-           toValue: options.startXY[1],
-           easing: Easing.bounce,
-           duration: 500,
-         }
-       ),
-     ]).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         state.opacity.setValue(0);
-       }
-       hop(options, state);
-     });
-   }
-
-   const tumbleOff = function(options, state) {
-     if (looping === false) {
-       return;
+   ).start();
+   state.top.setValue(options.startXY[1]);
+   Animated.timing(
+     state.top,
+     {
+       toValue: options.endXY[1],
+       easing: Easing.back(3),
+       duration: options.duration,
      }
-     state.left.setValue(options.startXY[0]);
-     state.top.setValue(options.startXY[1]);
-     state.rotation.setValue(0)
-     Animated.parallel([
-     Animated.timing(
-       state.left,
-       {
-         toValue: options.endXY[0],
-         easing: Easing.back(3),
-         duration: options.duration,
-       }
-     ),
-     Animated.timing(
-       state.top,
-       {
-         toValue: options.endXY[1],
-         easing: Easing.back(3),
-         duration: options.duration,
-       }
-     ),
-     Animated.timing(
-       state.rotation,
-       {
-         toValue: options.endXY[0],
-         easing: Easing.back(3),
-         duration: options.duration,
-       }
-     )
-   ]).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         state.opacity.setValue(0);
-       }
-       tumbleOff(options, state);
-     });
-
-   }
-
-   const spin = function(options, state) {
-     if (looping === false) {
-       return;
+   ).start(() => {
+     if (options.loop === false) {
+       looping = false;
      }
-     state.rotation.setValue(0);
-     Animated.timing(
-       state.rotation,
-       {
-         toValue: 100,
-         easing: Easing.linear,
-         duration: options.duration,
-       }
-     ).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         state.opacity.setValue(0);
-       }
-       spin(options, state);
-     });
-   }
+     zoom(options, state);
+   });
+ }
 
-   const hopForward = function(options, state) {
-     if (looping === false) {
-       return;
-     }
-     state.left.setValue(options.startXY[0]);
-     state.top.setValue(options.startXY[1]);
-     Animated.parallel([
-        Animated.sequence([
-           Animated.timing(
-             state.left,
-             {
-               toValue: options.endXY[0]/2,
-               easing: Easing.sin,
-               duration: options.duration/2,
-             }
-           ),
-           Animated.timing(
-             state.left,
-             {
-               toValue: options.endXY[0],
-               easing: Easing.sin,
-               duration: options.duration/2,
-               delay: 100,
-             }
-           ),
+  const hop = function(options, state) {
+
+    state.top.setValue(options.startXY[1]);
+    Animated.sequence([
+      Animated.timing(
+        state.top,
+        {
+          toValue: (options.startXY[1] - 75),
+          easing: Easing.linear,
+          duration: 100,
+        }
+      ),
+      Animated.timing(
+        state.top,
+        {
+          toValue: options.startXY[1],
+          easing: Easing.bounce,
+          duration: 500,
+        }
+      ),
+    ]).start(() => {
+      if (options.loop === false) {
+        return
+      }else{
+        hop(options, state);
+      }
+    });
+  }
+
+  const tumbleOff = function(options, state) {
+
+    state.left.setValue(options.startXY[0]);
+    state.top.setValue(options.startXY[1]);
+    state.rotation.setValue(0)
+    Animated.parallel([
+      Animated.timing(
+        state.left,
+        {
+          toValue: options.endXY[0],
+          easing: Easing.back(3),
+          duration: options.duration,
+        }
+      ),
+      Animated.timing(
+        state.top,
+        {
+          toValue: options.endXY[1],
+          easing: Easing.back(3),
+          duration: options.duration,
+        }
+      ),
+      Animated.timing(
+        state.rotation,
+        {
+          toValue: options.endXY[0],
+          easing: Easing.back(3),
+          duration: options.duration,
+        }
+      )
+    ]).start(() => {
+      if (options.loop === false) {
+        return
+      }else{
+        tumbleOff(options, state);
+      }
+    });
+  }
+
+  const spin = function(options, state) {
+
+    state.rotation.setValue(0);
+    Animated.timing(
+      state.rotation,
+      {
+        toValue: 100,
+        easing: Easing.linear,
+        duration: options.duration,
+      }
+    ).start(() => {
+      if (options.loop === false) {
+        return
+      }else{
+        spin(options, state);
+      }
+    });
+  }
+
+  const hopForward = function(options, state) {
+
+    state.left.setValue(options.startXY[0]);
+    state.top.setValue(options.startXY[1]);
+    Animated.parallel([
+      Animated.sequence([
+        Animated.timing(
+          state.left,
+          {
+            toValue: options.endXY[0]/2,
+            easing: Easing.sin,
+            duration: options.duration/2,
+          }
+        ),
+        Animated.timing(
+          state.left,
+          {
+            toValue: options.endXY[0],
+            easing: Easing.sin,
+            duration: options.duration/2,
+            delay: 100,
+          }
+        ),
+      ]),
+      Animated.sequence([
+          Animated.timing(
+            state.top,
+            {
+              toValue: options.yTo[0],
+              easing: Easing.linear,
+              duration: options.duration/4,
+            }
+          ),
+          Animated.timing(
+            state.top,
+            {
+              toValue: options.startXY[1],
+              easing: Easing.linear,
+              duration: options.duration/4,
+            }
+          ),
+          Animated.timing(
+            state.top,
+            {
+              toValue: options.yTo[0],
+              easing: Easing.linear,
+              duration: options.duration/4,
+              delay: 100,
+            }
+          ),
+          Animated.timing(
+            state.top,
+            {
+              toValue: options.startXY[1],
+              easing: Easing.linear,
+              duration: options.duration/4,
+            }
+          ),
         ]),
-        Animated.sequence([
-            Animated.timing(
-             state.top,
-             {
-               toValue: options.yTo[0],
-               easing: Easing.linear,
-               duration: options.duration/4,
-             }
-            ),
-            Animated.timing(
-             state.top,
-             {
-               toValue: options.startXY[1],
-               easing: Easing.linear,
-               duration: options.duration/4,
-             }
-           ),
-           Animated.timing(
-             state.top,
-             {
-               toValue: options.yTo[0],
-               easing: Easing.linear,
-               duration: options.duration/4,
-               delay: 100,
-             }
-            ),
-            Animated.timing(
-             state.top,
-             {
-               toValue: options.startXY[1],
-               easing: Easing.linear,
-               duration: options.duration/4,
-             }
-           ),
-        ]),
-     ]).start(() => {
-       if (options.loop === false) {
-         looping = false;
-       }
-       if (options.disappearAfterAnimation) {
-         state.opacity.setValue(0);
-       }
-       hopForward(options, state);
-     });
-   }
+      ]).start(() => {
+        if (options.loop === false) {
+          return
+        }else{
+          hopForward(options, state);
+        }
+      });
+  }
 
   return (
     {
@@ -511,7 +371,6 @@ const Tweener = function () {
       'slip-slide': move,
       'sine-wave': sineWave,
       'pulse': pulse,
-      'Looper': Looper,
       'wiggle': wiggle,
       'bounce-drop': bounceDrop,
       'zoom': zoom,
@@ -521,6 +380,7 @@ const Tweener = function () {
       'hop-forward': hopForward,
     }
   );
+
 };
 
 export default Tweener;
