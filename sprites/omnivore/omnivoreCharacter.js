c543775867ff2dc8bd504938a883c10087cfb8e3
Galen March <galenmarch@comcast.net>
2016-08-10 10:08:35 -0400

    integrated omnivore, goat animations

--- sprites/omnivore/omnivoreCharacter.js
+++ sprites/omnivore/omnivoreCharacter.js
@@ -7,6 +7,43 @@ const omnivoreCharacter = {
     require("./omnivore.png"),
   ],
 
+  celebrate:[
+    require("./monster_celebrate01.png"),
+    require("./monster_celebrate02.png"),
+    require("./monster_celebrate03.png"),
+    require("./monster_celebrate04.png"),
+    require("./monster_celebrate05.png"),
+    require("./monster_celebrate06.png"),
+    require("./monster_celebrate07.png"),
+    require("./monster_celebrate08.png"),
+    require("./monster_celebrate09.png"),
+    require("./monster_celebrate10.png"),
+  ],
+
+  walk:[
+    require("./monster_walk01.png"),
+    require("./monster_walk02.png"),
+    require("./monster_walk03.png"),
+    require("./monster_walk04.png"),
+    require("./monster_walk05.png"),
+    require("./monster_walk06.png"),
+  ],
+
+  eat:[
+    require("./monster_eat01.png"),
+    require("./monster_eat02.png"),
+    require("./monster_eat03.png"),
+    require("./monster_eat04.png"),
+    require("./monster_eat05.png"),
+    require("./monster_eat06.png"),
+  ],
+
+  disgust:[
+    require("./monster_disgust01.png"),
+    require("./monster_disgust02.png"),
+    require("./monster_disgust03.png"),
+  ],
+
   green:[
     require("./omnivore_green.png"),
   ],
